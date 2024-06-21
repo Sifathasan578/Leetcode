8315aa3629bf72f11b708259c49b755204dcd7ba
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-21 19:04:30 +0600

    Create lc242.cpp

---
+++ lc242.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define deb(n)       cout<<#n<<" = "<<n<<"\n";
+#define keepLearning return 0;
+
+bool isAnagram(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if(s == t) return true;
+    else return false;
+
+    // TC: O(n logn), logn for sorting
+}
+
+int main() {
+    string s, t; cin>>s>>t;
+    cout<<isAnagram(s, t)<<"\n";
+}
