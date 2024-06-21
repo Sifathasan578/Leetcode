cca75b5033ee00c0ea707388c9719d38e7f6ba3e
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-21 14:16:34 +0600

    Create lc169.cpp

---
+++ lc169.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define deb(n)       cout<<#n<<" = "<<n<<"\n";
+#define keepLearning return 0;
+
+int majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    map<int, int> mp;
+    int x = n / 2, res;
+
+    for (int i : nums) {
+        mp[i]++;
+        if (mp[i] > x) {
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> vec;
+    for (int i = 0; i < n; i++) {
+        int k; cin >> k;
+        vec.push_back(k);
+    }
+    cout << majorityElement(vec) << "\n";
+}
