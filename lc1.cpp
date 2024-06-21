dc0ef9366cbdd3ecabb0efc149589c14be77d075
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-21 18:50:45 +0600

    Create lc1.cpp

---
+++ lc1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define deb(n)       cout<<#n<<" = "<<n<<"\n";
+#define keepLearning return 0;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> mp, idx;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+        int x = nums[i];
+        int y = target - x;
+
+        if (mp.count(y) != 0) {
+            return { i, mp[y] };
+        }
+
+        mp[ nums[i] ] = i;
+    }
+
+    return {};
+
+  // TC: O(n logn), logn for map
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> vec1;
+    for (int i = 0; i < n; i++) {
+        int k; cin >> k;
+        vec1.push_back(k);
+    }
+    int val; cin >> val;
+    vector<int> res = twoSum(vec1, val);
+    for (int i : res) {
+        cout << i << " ";
+    }
+}
