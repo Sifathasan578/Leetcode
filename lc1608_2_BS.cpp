2d01c648caebbc3636b9fbc59a2e336a2e4af3cf
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-23 03:14:41 +0600

    Update lc1608_2_BS.cpp

--- lc1608_2_BS.cpp
+++ lc1608_2_BS.cpp
@@ -5,16 +5,18 @@ using namespace std;
 #define keepLearning return 0;
 
 int specialArray(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end()); // O(nlogn)
     int n = nums.size();
 
     for (int x = n; x >= 0; x--) {
-        int idx = lower_bound(nums.begin(), nums.end(), x) - nums.begin();
+        int idx = lower_bound(nums.begin(), nums.end(), x) - nums.begin(); // O(logn)
         int cnt = n - idx;
 
         if (cnt == x) return x;
     }
     return -1;
+
+    // TC: O(nlogn)
 }
 
 int main() {
