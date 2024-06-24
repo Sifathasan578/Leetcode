e272cc310406d186697d755381cc783a7db1f471
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-24 17:16:50 +0600

    Create lc912.cpp

---
+++ lc912.cpp
@@ -0,0 +1,16 @@
+// Requirement: TC: O(n logn), SC: As min as possible
+// https://leetcode.com/problems/sort-an-array/solutions/1401412/c-clean-code-solution-fastest-all-15-sorting-methods-detailed/
+// Insertion: O(n ^ 2)
+// Selection: O(n ^ 2)
+// Bubble: O(n ^ 2), worst
+// Quick: O(n ^ 2), worst
+// Merge: O(nlogn), inplace, outplace
+// Counting: Theta(max(n, m)) where m = max(Arr)
+// Generalised Radix: Theta(n)
+// Heap: O(nlogn), SC: O(1)
+// Merge sort, Heap Sort, Count Sort
+// I will solve this with HeapSort. TC: O(nlogn), SC: O(1) 
+// https://www.geeksforgeeks.org/heap-sort/ 
+// https://www.geeksforgeeks.org/binary-heap/ 
+// https://www.geeksforgeeks.org/difference-between-min-heap-and-max-heap/
+// PENDING OR On hold
