1296b19cd7c914b91342b7f327a76189c3c4dd2d
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-26 16:33:34 +0600

    Create lc128.cpp

---
+++ lc128.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define deb(n)       cout<<#n<<" = "<<n<<"\n";
+#define keepLearning return 0;
+
+int longestConsecutive(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+    int n = nums.size(), cnt = 1, res = 0;
+    for (int i = 1; i < n; i++) {
+        if ((nums[i - 1] + 1) == nums[i]) {
+            cnt++;
+            res = max(res, cnt);
+        }
+        else {
+            cnt = 1;
+        }
+    }
+    res = max(res, cnt);
+    if (n == 0) res = 0;
+    return res;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    int res = longestConsecutive(nums);
+    cout << res << "\n";
+}
