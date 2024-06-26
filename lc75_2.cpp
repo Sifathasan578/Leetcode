fba67c02492f2b3c6416bbe81ad7d48f6b256127
Imrul Hasan Sifat <55889347+Sifathasan578@users.noreply.github.com>
2024-06-26 18:04:11 +0600

    Create lc75_2.cpp

---
+++ lc75_2.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define deb(n)       cout<<#n<<" = "<<n<<"\n";
+#define keepLearning return 0;
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else { // == 2
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+
+    // TC: O(n)
+    // Dutch National Flag Algorithm by Edsger W
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    sortColors(nums);
+}
